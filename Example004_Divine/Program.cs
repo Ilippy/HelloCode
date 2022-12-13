3a73848aca6d71e817577fcdea8f4c0bfb263f90
Ilippy <Ilippyk@gmail.com>
2022-12-14 01:50:47 +0300

    add Example 004 Divine project

---
+++ Example004_Divine/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое дробное число через запятую");
+string numberA = Console.ReadLine();
+Console.WriteLine("Введите второе дробное число через запяту");
+string numberB = Console.ReadLine();
+double sum = double.Parse(numberA) / double.Parse(numberB);
+Console.WriteLine($"Деление чисел {numberA} и {numberB} равняется {sum}!");
