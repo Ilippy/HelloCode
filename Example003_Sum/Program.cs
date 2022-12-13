f39b5618c05e4afeeed8874a371b601e6ee4bedc
Ilippy <Ilippyk@gmail.com>
2022-12-14 02:39:47 +0300

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,12 @@
-﻿Console.WriteLine("Введите первое целое число");
-string numberA = Console.ReadLine();
-Console.WriteLine("Введите второе целое число");
-string numberB = Console.ReadLine();
-int sum = int.Parse(numberA) + int.Parse(numberB);
+﻿//Принимаем числа из консоли
+//Console.WriteLine("Введите первое целое число");
+//string numberA = Console.ReadLine();
+//Console.WriteLine("Введите второе целое число");
+//string numberB = Console.ReadLine();
+//int sum = int.Parse(numberA) + int.Parse(numberB);
+
+// Рандомно назначаем числа от 1 до 9
+int numberA = new Random().Next(1, 10); // 1,2,3,4 ... 9
+int numberB = new Random().Next(1, 10); // 1,2,3,4 ... 9
+int sum = numberA + numberB;
 Console.WriteLine($"Сумма чисел {numberA} и {numberB} равняется {sum}!");
