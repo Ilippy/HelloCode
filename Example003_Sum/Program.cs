7534bfc75878627e81df840762dc2d5072ab6b21
Ilippy <Ilippyk@gmail.com>
2022-12-14 01:38:26 +0300

    add Example 004 Sum project

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое целое число");
+string numberA = Console.ReadLine();
+Console.WriteLine("Введите второе целое число");
+string numberB = Console.ReadLine();
+int sum = int.Parse(numberA) + int.Parse(numberB);
+Console.WriteLine($"Сумма чисел {numberA} и {numberB} равняется {sum}!");
