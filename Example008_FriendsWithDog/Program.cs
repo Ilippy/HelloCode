05aa2fadc7641621ad30ffb8c737bd6c5e05db28
Ilippy <Ilippyk@gmail.com>
2022-12-14 03:40:44 +0300

    add Example 008 Friends with a dog project

---
+++ Example008_FriendsWithDog/Program.cs
@@ -0,0 +1,22 @@
+﻿double distance = 10000;
+int friendspeed1 = 1, friendspeed2 = 2, dogspeed = 5;
+double time;
+int count = 0;
+int friend = 2;
+
+while (distance >= 10)
+{
+	if (friend == 2)
+    {
+		time = distance / (friendspeed2 + dogspeed);
+		friend = 1;
+    }
+	else
+    {
+		time = distance / (friendspeed1 + dogspeed);
+		friend = 2;
+    }
+	distance = distance - (friendspeed1 + friendspeed2) * time;
+	count++;
+}
+Console.WriteLine($"Dog ran {count} times");
