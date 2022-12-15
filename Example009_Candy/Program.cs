b1461a195d2eaca3e0555fa0531adc9611d5d211
Ilippy <Ilippyk@gmail.com>
2022-12-16 01:34:41 +0300

    add Example 10 Test

--- Example009_Candy/Program.cs
+++ Example009_Candy/Program.cs
@@ -1,81 +1,91 @@
-﻿System.Console.WriteLine("Введите общее количество конфет");
-string stringCandyAmount = Console.ReadLine().Trim();
-int candyAmount;
-while(!int.TryParse(stringCandyAmount, out candyAmount))
+﻿using System;
+
+namespace Example009_Candy
 {
-    System.Console.WriteLine("Ошибка! Введите одно целое число");
-    stringCandyAmount = Console.ReadLine().Trim();
-}
-// candyAmount = enterInt(stringCandyAmount);
-
-// System.Console.WriteLine(candyAmount);
-
-System.Console.WriteLine("Введите количество конфет, которое игрок может взять за 1 ход");
-string stringMaxAmountOfCandyPerTurn = Console.ReadLine().Trim();
-int maxAmountOfCandyPerTurn;
-while(!int.TryParse(stringMaxAmountOfCandyPerTurn, out maxAmountOfCandyPerTurn))
-{
-    System.Console.WriteLine("Ошибка! Введите одно целое число");
-    stringMaxAmountOfCandyPerTurn = Console.ReadLine().Trim();
-}
-// amountOfCandyPerTurn = enterInt(stringAmountOfCandyPerTurn);
-// System.Console.WriteLine(amountOfCandyPerTurn);
-
-bool isPlayer = true;
-string stringCandyInThisTurn;
-int candyInThisTurn = 0;
-
-while(candyAmount > 0)
-{
-    if(isPlayer)
+    public class Candy
     {
-        System.Console.WriteLine($"Осталось {candyAmount} конфет(ы)");
-        System.Console.WriteLine($"Введите число конфет, которые вы хотите взять в этом ходу от 1 до {maxAmountOfCandyPerTurn}");
-        stringCandyInThisTurn = Console.ReadLine().Trim();
-        candyInThisTurn = 0;
-       
-            while(!int.TryParse(stringCandyInThisTurn, out candyInThisTurn) || candyInThisTurn <= 0 || candyInThisTurn > maxAmountOfCandyPerTurn)
+        static void Main(string[] args)
+        {
+            int candyAmount = EnterNumber("Введите общее количество конфет");
+            int maxAmountOfCandyPerTurn = EnterNumber("Введите количество конфет, которое игрок может взять за 1 ход");
+            bool isPlayer = true;
+            string stringCandyInThisTurn;
+            int candyInThisTurn = 0;
+
+            while (candyAmount > 0)
             {
-                System.Console.WriteLine($"Ошибка! Введите одно целое число от 1 до {maxAmountOfCandyPerTurn}");
-                stringCandyInThisTurn = Console.ReadLine().Trim();
+                if (isPlayer)
+                {
+                    System.Console.WriteLine($"Осталось {candyAmount} конфет(ы)");
+                    System.Console.WriteLine($"Введите число конфет, которые вы хотите взять в этом ходу от 1 до {maxAmountOfCandyPerTurn}");
+                    stringCandyInThisTurn = Console.ReadLine().Trim();
+                    candyInThisTurn = 0;
+
+                    while (!int.TryParse(stringCandyInThisTurn, out candyInThisTurn) || candyInThisTurn <= 0 || candyInThisTurn > maxAmountOfCandyPerTurn)
+                    {
+                        System.Console.WriteLine($"Ошибка! Введите одно целое число от 1 до {maxAmountOfCandyPerTurn}");
+                        stringCandyInThisTurn = Console.ReadLine().Trim();
+                    }
+
+                    candyAmount -= candyInThisTurn;
+                    isPlayer = false;
+                }
+                else
+                {
+                    System.Console.WriteLine($"Осталось {candyAmount} конфет(ы)");
+                    candyInThisTurn = candyAmount % (maxAmountOfCandyPerTurn + 1);
+                    if (candyInThisTurn == 0)
+                    {
+                        candyInThisTurn = new Random().Next(1, maxAmountOfCandyPerTurn + 1);
+                    }
+                    candyAmount -= candyInThisTurn;
+                    System.Console.WriteLine($"Компьютер взял {candyInThisTurn} конфет(ы)");
+                    isPlayer = true;
+                }
+                if (candyAmount == 0)
+                {
+                    if (isPlayer)
+                    {
+                        System.Console.WriteLine("Победил КОМПЬЮТЕР! Ну как всегда =)");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Победил игрок. Ну наконец-то =D");
+                    }
+                }
             }
-        
-        candyAmount -= candyInThisTurn;
-        isPlayer = false;
-    } else 
-    {
-        System.Console.WriteLine($"Осталось {candyAmount} конфет(ы)");
-        candyInThisTurn = candyAmount % (maxAmountOfCandyPerTurn + 1);
-        if (candyInThisTurn == 0)
-        {
-            candyInThisTurn = new Random().Next(1,maxAmountOfCandyPerTurn + 1);
+
         }
-        candyAmount -= candyInThisTurn;
-        System.Console.WriteLine($"Компьютер взял {candyInThisTurn} конфет(ы)");
-        isPlayer = true;
-    }
-    if(candyAmount == 0)
-    {
-        if(isPlayer)
+
+        static int EnterNumber(string str)
         {
-            System.Console.WriteLine("Победил КОМПЬЮТЕР! Ну как всегда =)");
-        } else {
-            System.Console.WriteLine("Победил игрок. Ну наконец-то =D");
+            System.Console.WriteLine(str);
+            string stringNumber = Console.ReadLine().Trim();
+            int number;
+            while (!int.TryParse(stringNumber, out number) || number <= 0)
+            {
+                Console.WriteLine("Ошибка! Введите одно целое число");
+                stringNumber = Console.ReadLine().Trim();
+            }
+            return number;
         }
+
+        // static int enterInt(string str);
+        // System.Console.WriteLine(str);
+        // int number;
+        // {
+        //     while(!int.TryParse(str, out number))
+        //     {
+        //         System.Console.WriteLine("Ошибка! Введите одно целое число");
+        //         str = Console.ReadLine().Trim();
+        //     }
+        //     return number;
+        // }
+
     }
 }
 
 
 
 
-// int enterInt(string str);
-// System.Console.WriteLine(str);
-// int number;
-// {
-//     while(!int.TryParse(str, out number))
-//     {
-//         System.Console.WriteLine("Ошибка! Введите одно целое число");
-//         str = Console.ReadLine().Trim();
-//     }
-//     return number;
-// }
+
---
+++ Example010_Test/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+Console.WriteLine("Введите число");
+string stringNumber = Console.ReadLine();
+int number;
+while (!int.TryParse(stringNumber, out number))
+{
+    Console.WriteLine("Ошибка! Введите одно целое число");
+    stringNumber = Console.ReadLine().Trim();
+}
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());  //int.Parse()
+int result = number * number; 
+
+Console.WriteLine($"Квадрат числа {number} будет {result}");  
+// Console.WriteLine("Квадрат числа " + {number} + " будет "+ result);
