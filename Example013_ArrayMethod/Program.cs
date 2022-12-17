6ab75b41f78f9976b8a58e28582d84cbe99d67a1
Ilippy <Ilippyk@gmail.com>
2022-12-17 13:00:06 +0300

    add Example 013 Method Array project

---
+++ Example013_ArrayMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 32, 12, 63, 43, 52, 46, 97, 58, 19 };
+
+        int n = array.Length;
+        int find = array[new Random().Next(0,array.Length)];
+        System.Console.WriteLine(find);
+
+        int index = 0;
+
+        while(index < n)
+        {
+            if(array[index] == find){
+                System.Console.WriteLine(index);
+                break;
+            }
+            index++;
+        }
+    }
+}
